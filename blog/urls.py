6433d5ac86a8ebc34f41ce791b9f2cb905bb14f4
Rubel Mahmud <rmsoft.com@gmail.com>
2020-12-21 02:01:53 -0800

    modify template for show post_draft_list, add publish and remove button

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('post/<int:pk>/', post_detail, name='post_detail'),
     path('post/new/', post_new, name='post_new'),
     path('post/<int:pk>/edit/', post_edit, name='post_edit'),
-    path('post/<int:pk>/delete/', post_delete, name='post_delete'),
-    path('post/draft_list/', post_draft_list, name='post_draft_list'),
-    path('post/<int:pk>/publish/', post_publish, name='post_publish'),
+    path('drafts/', post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', post_publish, name='post_publish'),
+    path('post/<pk>/remove/', post_remove, name='post_remove'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -52,16 +52,9 @@ def post_edit(request, pk):
     })
 
 
-
-def post_delete(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    post.delete()
-    return redirect('post_list')
-
-
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull = True).order_by('created_date')
-    return render(request, 'blog/post_list.html', {
+    return render(request, 'blog/post_draft_list.html', {
         'posts':posts
     })
 
@@ -69,4 +62,11 @@ def post_draft_list(request):
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
     return redirect('post_list')
