0296a6d3688fcdcfed78a7ef72067955a2b86ed6
Rubel Mahmud <rmsoft.com@gmail.com>
2020-12-21 01:10:39 -0800

    add remove and publish button

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from blog.models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'published_date']
+
+admin.site.register(Post, PostAdmin)
--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from blog.views import post_list, post_detail, post_new, post_edit
+from blog.views import *
 
 urlpatterns = [
     path('', post_list, name='post_list'),
     path('post/<int:pk>/', post_detail, name='post_detail'),
     path('post/new/', post_new, name='post_new'),
     path('post/<int:pk>/edit/', post_edit, name='post_edit'),
+    path('post/<int:pk>/delete/', post_delete, name='post_delete'),
+    path('post/draft_list/', post_draft_list, name='post_draft_list'),
+    path('post/<int:pk>/publish/', post_publish, name='post_publish'),
 ]
