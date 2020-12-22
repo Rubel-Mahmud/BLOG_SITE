43c48a9b038abb6edcd6c80c10ed8076a80d1988
Rubel Mahmud <rmsoft.com@gmail.com>
2020-12-22 10:40:10 -0800

    add comment feature and finish the project

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('drafts/', post_draft_list, name='post_draft_list'),
     path('post/<pk>/publish/', post_publish, name='post_publish'),
     path('post/<pk>/remove/', post_remove, name='post_remove'),
+    path('post/<int:pk>/comment/', add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', comment_remove, name='comment_remove'),
 ]
